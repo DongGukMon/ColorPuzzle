2426643b2c6810114f654d1197132a613cdfd02b
DongGukMon <ehdrnr9346@naver.com>
2022-10-04 16:13:38 +0900

    app icon 및 splash 이미지 적용

--- App.tsx
+++ App.tsx
@@ -8,16 +8,23 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useColorScheme} from 'react-native';
 import {RecoilRoot} from 'recoil';
 import {ThemeProvider} from 'styled-components/native';
 import {darkTheme, lightTheme} from './src/theme';
 import AppRoot from './src/screen/AppRoot';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
   const theme = isDarkMode ? darkTheme : lightTheme;
+
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
--- android/app/src/main/java/com/recoilpuzzle/MainActivity.java
+++ android/app/src/main/java/com/recoilpuzzle/MainActivity.java
@@ -1,8 +1,13 @@
 package com.recoilpuzzle;
 
+import android.os.Bundle; // add
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+
+
+import org.devio.rn.splashscreen.SplashScreen; 
 
 public class MainActivity extends ReactActivity {
 
@@ -15,6 +20,12 @@ public class MainActivity extends ReactActivity {
     return "RecoilPuzzle";
   }
 
+
+       @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this); // 추가해주세요.
+    super.onCreate(savedInstanceState);
+  }
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
    * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
@@ -22,6 +33,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
+    
     return new MainActivityDelegate(this, getMainComponentName());
   }
 
@@ -30,9 +42,13 @@ public class MainActivity extends ReactActivity {
       super(activity, mainComponentName);
     }
 
+
+
+
     @Override
     protected ReactRootView createRootView() {
       ReactRootView reactRootView = new ReactRootView(getContext());
+      
       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
       reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
       return reactRootView;
@@ -40,6 +56,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected boolean isConcurrentRootEnabled() {
+      
       // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
